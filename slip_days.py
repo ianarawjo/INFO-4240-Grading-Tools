638c5bc4c74e3e4adabb763c59ff788184c790fb
Ian Arawjo <fatso784@gmail.com>
2021-05-21 11:50:17 -0400

    slip day edit

--- slip_days.py
+++ slip_days.py
@@ -9,6 +9,7 @@ import pandas as pd
 # If you need to add to excluding, you put the key of the rubric_data_map (below)
 # corresponding to the assignment you wish to exclude. Multiple can be comma-separated (no spaces).
 PATH_TO_EXTRA_SLIP_DAYS_CSV = 'data/extra_slip_days.csv' # Set to None if you don't have this
+SAVE_TO = 'data/slip_days.csv'
 
 # Map rubric files to directories containing all assignment eval sheets from GS.
 # This assumes the json rubrics are in /rubrics and the assignment folders are in the /data folder.
@@ -20,9 +21,10 @@ rubric_data_map = {
     'dw2': 'dw2',
     'dw3': 'dw3',
     'mp3_indiv': 'mp3/indiv',
-    # 'mp3_group': 'mp3/group',
+    'mp3_group': 'mp3/group',
     'mp4': 'mp4',
-    'dw4': 'dw4'
+    'dw4': 'dw4',
+    'dw5': 'dw5'
 }
 INITIAL_SLIPS = 10 # the number of slip days every student starts with
 
@@ -38,6 +40,7 @@ if PATH_TO_EXTRA_SLIP_DAYS_CSV:
 # special_check = {}
 slip_days = {}
 emails_to_names = {}
+mp3_slips_used = {}
 for rubric_name, dir_name in rubric_data_map.items():
     grades, rubric, questions = load_grades(os.path.join('rubrics', rubric_name+'.json'), \
                                             os.path.join('data', dir_name), \
@@ -52,15 +55,24 @@ for rubric_name, dir_name in rubric_data_map.items():
                 num_slips_used = 0 # override lateness for exclusions
                 print("Excluding", dir_name, "from student", email, "slip days")
             elif g['late'] > 0:
+                # Calculate *how* late (in days, 24hr periods=1440 min)
                 num_slips_used = int(g['late'] / 1440)+1
             else:
                 num_slips_used = 0
 
-            # Calculate *how* late (in days, 24hr periods=1440 min)
-            if email in slip_days:
-                slip_days[email] += num_slips_used
+            if 'mp3' in rubric_name:
+                if email in mp3_slips_used: # special case for mp3
+                    # when seeing the second mp3, now add the max to slip days count
+                    slip_days[email] += max(mp3_slips_used[email], num_slips_used)
+                    print('Added mp3 slips for', email,  max(mp3_slips_used[email], num_slips_used))
+                else:
+                    # don't add the first mp3 assignment to slip days count yet
+                    mp3_slips_used[email] = num_slips_used
             else:
-                slip_days[email] = num_slips_used
+                if email in slip_days:
+                    slip_days[email] += num_slips_used
+                else:
+                    slip_days[email] = num_slips_used
             seen_sids[email] = True
 
 # Read extra slip days sheet, and subtract from the total used:
@@ -79,5 +91,5 @@ for email, used_slips in slip_days.items():
     print(email, slips)
     rem_slips.append( [emails_to_names[email], email, slips] )
 df_slips = pd.DataFrame(rem_slips, columns=["Name", "Email", "Slip Days Remaining"])
-df_slips.to_csv("slip_days.csv", index=False)
-print("Saved remaining slip days to spreadsheet slip_days.csv.")
+df_slips.to_csv(SAVE_TO, index=False)
+print("Saved remaining slip days to spreadsheet", SAVE_TO)
